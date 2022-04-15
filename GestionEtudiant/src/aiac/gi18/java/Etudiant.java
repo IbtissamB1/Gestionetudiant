287fa8f621e71016f032998962b9393ea4a1abd6
ibtis <ibtis@DESKTOP-JDOA2H4>
2022-04-15 10:57:08 +0000

    ..

---
+++ GestionEtudiant/src/aiac/gi18/java/Etudiant.java
@@ -0,0 +1,10 @@
+package aiac.gi18.java;
+
+public class Etudiant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ GestionEtudiant/src/aiac/gi18/java/main.java
@@ -0,0 +1,10 @@
+package aiac.gi18.java;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println(".");
+	}
+
+}
